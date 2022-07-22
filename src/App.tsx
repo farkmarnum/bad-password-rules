013ea78f416e483ccdfcc0c5f33d553eca0127c5
farkmarnum <mark@nayya.com>
2022-07-22 13:10:38 -0400

    working v1 w/ no validations

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import PasswordInput from './components/PasswordInput';
 
 const App = () => {
   const [password, setPassword] = useState('');
-  const [shouldShowPassword, setShouldShowPassword] = useState(false);
-  const togglePasswordVisibility = () => setShouldShowPassword((x) => !x);
-
-  const [cursor, setCursor] = useState<number | null>(null);
-
-  const ref = useRef<any>(null);
-
+  // console.log(password);
   // const errors = deriveErrors(password);
 
   return (
@@ -18,82 +13,8 @@ const App = () => {
         <h1>Bad Password Rules</h1>
       </header>
       <div className="main">
-        <input
-          className="password-input"
-          type="text"
-          value={password}
-          onKeyPress={(evt) => {
-            const { target, key } = evt;
-            const { selectionStart, selectionEnd } = target;
-            const getNewValue = (value: string) =>
-              [
-                value.slice(0, selectionStart || 0),
-                key,
-                value.slice(selectionEnd || 0 + 1),
-              ].join('');
+        <PasswordInput setPassword={setPassword} />
 
-            cursor.current = selectionStart;
-            console.log('storing:', cursor.current);
-            setPassword(getNewValue);
-          }}
-          onKeyDown={(evt) => {
-            switch (evt.key) {
-              case 'Unidentified':
-                return;
-
-              case 'Backspace': {
-                const { target } = evt;
-                const { selectionStart, selectionEnd } = target;
-
-                let start = selectionStart || 0;
-                const end = selectionEnd || 0;
-
-                // Handle case where there is no selection:
-                if (start === end) start -= 1;
-
-                cursor.current = start;
-                console.log('storing:', cursor.current);
-                setPassword((value) =>
-                  [value.slice(0, start), value.slice(end)].join(''),
-                );
-                break;
-              }
-              case 'Delete': {
-                const { target } = evt;
-                const { selectionStart, selectionEnd } = target;
-
-                const start = selectionStart || 0;
-                let end = selectionEnd || 0;
-
-                // Handle case where there is no selection:
-                if (start === end) end += 1;
-
-                cursor.current = start;
-                console.log('storing:', cursor.current);
-                setPassword((value) =>
-                  [value.slice(0, start), value.slice(end)].join(''),
-                );
-                break;
-              }
-              default:
-                // console.log(evt.key);
-                break;
-            }
-          }}
-          onFocus={(evt) => {
-            console.log('loading:', cursor.current);
-            evt.target.selectionStart = cursor.current || 0;
-          }}
-        />
-        <br />
-
-        <button
-          className="text-btn"
-          type="button"
-          onClick={togglePasswordVisibility}
-        >
-          {shouldShowPassword ? 'Hide' : 'Show'} Password
-        </button>
         {/* <div className="errors">(errors here)</div> */}
       </div>
     </div>
---
+++ src/components/PasswordInput.tsx
@@ -0,0 +1,171 @@
+import { useState, useRef, useEffect } from 'react';
+import eyeOpened from '../assets/eye-opened.png';
+import eyeClosed from '../assets/eye-closed.png';
+import './PasswordInput.css';
+
+const PasswordInput = ({
+  setPassword: setPasswordParent,
+}: {
+  setPassword: (arg0: string) => void;
+}) => {
+  const password = useRef('');
+  const setPassword = (newValue: string) => {
+    password.current = newValue;
+    setPasswordParent(newValue);
+  };
+
+  const ref = useRef<any>(null);
+
+  const [shouldShowPassword, setShouldShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => setShouldShowPassword((x) => !x);
+
+  const updateInput = (value: string, cursor: number) => {
+    const input = ref.current;
+
+    if (input) {
+      input.value = value;
+      input.setSelectionRange(cursor, cursor);
+    }
+  };
+
+  const updatePassword = (newValue: string, newCursor: number) => {
+    setPassword(newValue);
+
+    const displayValue = shouldShowPassword
+      ? newValue
+      : '•'.repeat(newValue.length);
+
+    updateInput(displayValue, newCursor);
+  };
+
+  useEffect(() => {
+    const input = ref.current;
+    if (input) {
+      const displayValue = shouldShowPassword
+        ? password.current
+        : '•'.repeat(password.current.length);
+
+      input.value = displayValue;
+    }
+  }, [shouldShowPassword]);
+
+  const handleKeyPress = (evt: React.KeyboardEvent<HTMLInputElement>) => {
+    evt.preventDefault();
+
+    const { target, key } = evt;
+    const { selectionStart, selectionEnd } = target;
+
+    const start = selectionStart || 0;
+    const end = selectionEnd || 0 + 1;
+
+    const newValue = [
+      password.current.slice(0, start),
+      key,
+      password.current.slice(end),
+    ].join('');
+
+    const newCursor = start + 1;
+    updatePassword(newValue, newCursor);
+  };
+
+  const handleKeyDown = (evt: React.KeyboardEvent<HTMLInputElement>) => {
+    const modifiers = ['Alt', 'Meta', 'Control'].reduce((acc, modifier) => {
+      acc[modifier.toLowerCase()] = evt.getModifierState(modifier);
+      return acc;
+    }, {} as Record<string, boolean>);
+
+    // TODO: support non-mac OS
+    const shouldDeleteWord = modifiers.alt || modifiers.control;
+    const shouldDeleteAll = modifiers.meta;
+
+    const { target } = evt;
+    const { selectionStart, selectionEnd } = target;
+
+    let start = selectionStart || 0;
+    let end = selectionEnd || 0;
+
+    switch (evt.key) {
+      case 'Unidentified':
+        return;
+
+      case 'Backspace': {
+        evt.preventDefault();
+
+        // Handle case where there is no selection:
+        if (start === end) {
+          if (shouldDeleteWord) {
+            const previousSpaceIndex = password.current
+              .slice(0, start)
+              .lastIndexOf(' ');
+            start = previousSpaceIndex;
+          } else if (shouldDeleteAll) {
+            start = 0;
+          } else {
+            start -= 1;
+          }
+        }
+
+        const newCursor = start;
+
+        const newValue = [
+          password.current.slice(0, start),
+          password.current.slice(end),
+        ].join('');
+
+        updatePassword(newValue, newCursor);
+        break;
+      }
+      case 'Delete': {
+        evt.preventDefault();
+        // Handle case where there is no selection:
+        if (start === end) {
+          if (shouldDeleteWord) {
+            const previousSpaceIndex = password.current
+              .slice(0, start)
+              .lastIndexOf(' ');
+            start = previousSpaceIndex;
+          } else if (shouldDeleteAll) {
+            start = 0;
+          } else {
+            end += 1;
+          }
+        }
+
+        const newValue = [
+          password.current.slice(0, start),
+          password.current.slice(end),
+        ].join('');
+
+        const newCursor = start;
+        updatePassword(newValue, newCursor);
+        break;
+      }
+      default:
+        break;
+    }
+  };
+
+  return (
+    <div className="container">
+      <input
+        ref={ref}
+        type="text"
+        onKeyPress={handleKeyPress}
+        onKeyDown={handleKeyDown}
+      />
+      <button
+        className="toggle-visibility-btn"
+        type="button"
+        onClick={togglePasswordVisibility}
+      >
+        <img
+          alt={shouldShowPassword ? 'Hide password' : 'Show password'}
+          src={shouldShowPassword ? eyeClosed : eyeOpened}
+        />
+      </button>
+    </div>
+  );
+};
+
+export default PasswordInput;
