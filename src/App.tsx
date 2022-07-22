354a263fccd19331387e45a6fbd89964cbc9364c
farkmarnum <mark@nayya.com>
2022-07-22 11:52:34 -0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './App.css';
 
 const App = () => {
   const [password, setPassword] = useState('');
   const [shouldShowPassword, setShouldShowPassword] = useState(false);
   const togglePasswordVisibility = () => setShouldShowPassword((x) => !x);
+
+  const [cursor, setCursor] = useState<number | null>(null);
+
+  const ref = useRef<any>(null);
 
   // const errors = deriveErrors(password);
 
@@ -16,24 +20,69 @@ const App = () => {
       <div className="main">
         <input
           className="password-input"
-          // autoComplete="off" // No password managers please
-          // data-lpignore="true" // Make LastPass ignore this field
           type="text"
           value={password}
-          onKeyPress={(evt) => setPassword(evt.key)}
-          onKeyDown={(evt) => {
-            if (evt.key === 'Unidentified') return;
+          onKeyPress={(evt) => {
+            const { target, key } = evt;
+            const { selectionStart, selectionEnd } = target;
+            const getNewValue = (value: string) =>
+              [
+                value.slice(0, selectionStart || 0),
+                key,
+                value.slice(selectionEnd || 0 + 1),
+              ].join('');
 
-            if (evt.key === 'Backspace') {
-              const { target } = evt;
-              const { selectionStart, selectionEnd } = target;
-              setPassword((value) =>
-                [
-                  value.slice(0, selectionStart || 0),
-                  value.slice(selectionEnd || 0 + 1),
-                ].join(''),
-              );
+            cursor.current = selectionStart;
+            console.log('storing:', cursor.current);
+            setPassword(getNewValue);
+          }}
+          onKeyDown={(evt) => {
+            switch (evt.key) {
+              case 'Unidentified':
+                return;
+
+              case 'Backspace': {
+                const { target } = evt;
+                const { selectionStart, selectionEnd } = target;
+
+                let start = selectionStart || 0;
+                const end = selectionEnd || 0;
+
+                // Handle case where there is no selection:
+                if (start === end) start -= 1;
+
+                cursor.current = start;
+                console.log('storing:', cursor.current);
+                setPassword((value) =>
+                  [value.slice(0, start), value.slice(end)].join(''),
+                );
+                break;
+              }
+              case 'Delete': {
+                const { target } = evt;
+                const { selectionStart, selectionEnd } = target;
+
+                const start = selectionStart || 0;
+                let end = selectionEnd || 0;
+
+                // Handle case where there is no selection:
+                if (start === end) end += 1;
+
+                cursor.current = start;
+                console.log('storing:', cursor.current);
+                setPassword((value) =>
+                  [value.slice(0, start), value.slice(end)].join(''),
+                );
+                break;
+              }
+              default:
+                // console.log(evt.key);
+                break;
             }
+          }}
+          onFocus={(evt) => {
+            console.log('loading:', cursor.current);
+            evt.target.selectionStart = cursor.current || 0;
           }}
         />
         <br />
